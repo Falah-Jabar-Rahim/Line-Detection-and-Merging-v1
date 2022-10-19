161bb50dd4e16a60342bb9b6467a6660f6690bba
Falah Jabar <falah.jabar22@yahoo.com>
2022-10-19 21:23:58 +0100

    Update intersection.m

--- intersection.m
+++ intersection.m
@@ -1,6 +1,7 @@
 function X = intersection(m1,b1,m2,b2)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+
+% Line detection and merging
+% (c) Falah Jabar (falah.jabar@lx.it.pt)
 
 A = [1 -m1; 1 -m2];
 B = transpose([b1 b2]);
